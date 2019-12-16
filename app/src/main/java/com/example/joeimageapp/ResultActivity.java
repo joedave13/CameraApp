5719e12db6079b9183b347fb9791d3966b4b7c75
Joshua Davian <44218146+joedave13@users.noreply.github.com>
2019-12-16 14:25:37 +0700

    Adding Little Feature and App Logo

--- app/src/main/java/com/example/joeimageapp/ResultActivity.java
+++ app/src/main/java/com/example/joeimageapp/ResultActivity.java
@@ -1,17 +1,22 @@
 package com.example.joeimageapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
     ImageView imageCamera;
-    Button btnBackToCamera;
+    Button btnBackToCamera, btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +25,30 @@ public class ResultActivity extends AppCompatActivity {
 
         imageCamera = findViewById(R.id.image_result);
         btnBackToCamera = findViewById(R.id.btn_back_camera);
+        btnSave = findViewById(R.id.btn_save_image);
 
-        Bundle extras = getIntent().getExtras();
+        final Bundle extras = getIntent().getExtras();
         if (extras != null){
             Bitmap img = (Bitmap)extras.get("image");
             if (imageCamera != null){
                 imageCamera.setImageBitmap(img);
             }
         }
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bitmap savedImage = (Bitmap)extras.get("image");
+                String savedImageUrl = MediaStore.Images.Media.insertImage(
+                        getContentResolver(),
+                        savedImage,
+                        "example_image",
+                        "example_image_camera"
+                );
+                Uri savedImageURI = Uri.parse(savedImageUrl);
+                Toast.makeText(ResultActivity.this, "Image saved to gallery", Toast.LENGTH_LONG).show();
+            }
+        });
 
         btnBackToCamera.setOnClickListener(new View.OnClickListener() {
             @Override
